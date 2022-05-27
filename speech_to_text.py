45388151b71bfa44acd6122b5813cc7f58d30d2f
daaa02 <dyk98498@gmail.com>
2022-05-27 10:29:49 +0900

    Update speech_to_text.py

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,51 +1,185 @@
-import io
-import os
-import requests
-import json
-import wave
+#!/usr/bin/env python
+
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Google Cloud Speech API sample application using the streaming API.
+
+NOTE: This module requires the additional dependency `pyaudio`. To install
+using pip:
+
+    pip install pyaudio
+
+Example usage:
+    python transcribe_streaming_mic.py
+"""
+
+# [START speech_transcribe_streaming_mic]
+from __future__ import division
+
+import re
+import sys
+
+from google.cloud import speech
+
+import pyaudio
+from six.moves import queue
+
+###############################
+from ctypes import *
+
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
 
 
-def isNumber(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
+def py_error_handler(filename, line, function, err, fmt):
+    return
 
 
-class TextToSpeech:
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+asound = cdll.LoadLibrary('libasound.so')
+# Set error handler
+asound.snd_lib_error_set_handler(c_error_handler)
+# Initialize PyAudio
+###############################
 
-    def tts_connection(self, string, filename="tts.wav"):
-        # Kakao auth-key
-        REST_API_KEY = "f8f8c3f66bb3310016fdeccffba152e8"
-        KAKAO_URL = "https://kakaoi-newtone-openapi.kakao.com/v1/synthesize"
-        HEADERS = {
-            "Content-Type": "application/xml",
-            "Authorization": "KakaoAK " + REST_API_KEY  # "KakaoAK" + "공백" + Key ;;;;; 
-        }
-        res = requests.post(KAKAO_URL, headers=HEADERS, data=string.encode('utf-8'))
+# Audio recording parameters
+RATE = 16000
+CHUNK = int(RATE / 10)  # 100ms
 
-        # 음성 합성 결과를 파일로 저장
-        with open(filename, "wb") as f:
-            f.write(res.content)
-            
-            
-    # tts, 효과음 등 모든 오디오를 플레이하는 함수
-    def play(self, filename, out='local', volume='-2000', background=True):
-        if not os.path.isfile(filename):
-            raise Exception(f'"{filename}" does not exist')
 
-        if not filename.split('.')[-1] in ['mp3', 'wav']:
-            raise Exception(f'"{filename}" must be (mp3|wav)')
+class MicrophoneStream(object):
+    """Opens a recording stream as a generator yielding the audio chunks."""
 
-        if not out in ['local', 'hdmi', 'both']:
-            raise Exception(f'"{out}" must be (local|hdmi|both)')
+    def __init__(self, rate, chunk):
+        self._rate = rate
+        self._chunk = chunk
 
-        if not isNumber(volume):
-            raise Exception(f'"{volume}" is not Number')
+        # Create a thread-safe buffer of audio data
+        self._buff = queue.Queue()
+        self.closed = True
 
-        if type(background) != bool:
-            raise Exception(f'"{background}" is not bool')
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            # The API currently only supports 1-channel (mono) audio
+            # https://goo.gl/z757pE
+            channels=1, rate=self._rate,
+            input=True, frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
 
-        opt = '&' if background else ''
-        os.system(f'omxplayer -o {out} --vol {volume} {filename} {opt}')
+        self.closed = False
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        self._audio_interface.terminate()
+
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        """Continuously collect data from the audio stream, into the buffer."""
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self):
+        while not self.closed:
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+
+            yield b''.join(data)
+
+
+def listen_print_loop(responses):
+    text = ''
+    num_chars_printed = 0
+    for response in responses:
+        if not response.results:
+            continue
+
+        result = response.results[0]
+        if not result.alternatives:
+            continue
+
+        transcript = result.alternatives[0].transcript
+        overwrite_chars = ' ' * (num_chars_printed - len(transcript))
+
+        if not result.is_final:
+            sys.stdout.write(transcript + overwrite_chars + '\r')
+            sys.stdout.flush()
+            num_chars_printed = len(transcript)
+        else:
+            text = transcript + overwrite_chars
+            break
+            if re.search(r'\b(exit|quit)\b', transcript, re.I):
+                print('Exiting..')
+                break
+            num_chars_printed = 0
+    return text
+
+
+def speech_to_text():
+    # See http://g.co/cloud/speech/docs/languages
+    # for a list of supported languages.
+    language_code = 'ko-KR'  # a BCP-47 language tag
+
+    client = speech.SpeechClient()
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=RATE,
+        language_code=language_code)
+    streaming_config = speech.StreamingRecognitionConfig(
+        config=config,
+        interim_results=True)
+
+    with MicrophoneStream(RATE, CHUNK) as stream:
+        audio_generator = stream.generator()
+        print("\n 답변을 시작해주세요. ")
+        requests = (speech.StreamingRecognizeRequest(audio_content=content)
+                    for content in audio_generator)
+
+        responses = client.streaming_recognize(streaming_config, requests)
+
+        # Now, put the transcription responses to use.
+        stt_out = listen_print_loop(responses)
+        print('\n')
+    return stt_out
+
+
+if __name__ == '__main__':
+    speech_to_text()
+# [END speech_transcribe_streaming_mic]
